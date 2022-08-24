bd5593276541aa182222b91b194e45221137c07c
belmino <belminoj@gmail.com>
2022-08-24 19:23:11 -0300

    webpack and babel
    webpack structure with public directory and babel in backend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,49 +1,18 @@
 /* eslint-disable */
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'main.js',
+    clean: true,
   },
   module: {
     rules: [
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpg|png)$/i,
-        loader: 'file-loader',
-        options: {
-          name: './images/[name].[ext]',
-        },
-      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: true,
-        keepClosingSlash: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: './bundle.css',
-    }),
   ],
 };
