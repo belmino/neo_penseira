220fe3dcab5e6d5a4eaa19495e5bc6e30f867ae7
belmino <belminoj@gmail.com>
2022-09-27 20:06:08 -0300

    loading e popup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import Error404Screen from './screens/Error404Screen.js';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import SigninScreen from './screens/SigninScreen.js';
-import { parseRequestUrl } from './utils.js';
+import { hideLoading, parseRequestUrl, showLoading } from './utils.js';
 
 const routes = {
   '/': HomeScreen,
@@ -16,6 +16,7 @@ const routes = {
 };
 
 const router = async () => {
+  // showLoading();
   const request = parseRequestUrl();
   const parseUrl = (request.resource ? `/${request.resource}` : '/')
     + (request.id ? '/:id' : '')
@@ -29,6 +30,7 @@ const router = async () => {
   const main = document.getElementById('main-container');
   main.innerHTML = await screen.render();
   await screen.after_render();
+  // hideLoading();
 };
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
